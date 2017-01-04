569edc4b8da251b8969cb9a92e02161bd424fc95
Afshin Arani <afshinarani79@gmail.com>
2017-01-04 13:19:22 +0330

    k3gxx: add Script to initialize WIFI & MODEM
    Note: I know it's a bad hack and i will try use wifiloader asap

--- device.mk
+++ device.mk
@@ -92,6 +92,7 @@ PRODUCT_PACKAGES += \
 ###########################################################
 
 PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/init.d/10wifi:system/etc/init.d/10wifi \
 	$(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
 	$(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
