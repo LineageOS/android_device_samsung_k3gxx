cc182dae7d92be7856305d996d967d0e361795ee
somadsul <somnathadsul@gmail.com>
2017-04-04 21:35:23 +0530

    k3gxx: Set Correct Path for mixer_path.xml
    Change-Id: Ib5db8a29051266a2f5342ccab8f64bffeafe95e6

--- device.mk
+++ device.mk
@@ -140,7 +140,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
 	$(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+	$(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
 
 PRODUCT_PACKAGES += \
 	audio.a2dp.default \
