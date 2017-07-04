62479bc4fe974181ba257e8c80ac4f08e00bceb0
hunny <hunnymadaanhunny@gmail.com>
2017-07-04 23:05:01 +0530

    update wifi configs

--- device.mk
+++ device.mk
@@ -234,7 +234,6 @@ PRODUCT_PACKAGES += \
 
 # WIFI
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
