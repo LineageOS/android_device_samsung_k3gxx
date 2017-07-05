a736e02ae49503dd9575b2ab7e7e18f454d9dc76
somadsul <somnathadsul@gmail.com>
2017-07-05 21:12:11 +0530

    k3gxx: ramdisk: add init.baseband.rc
    Change-Id: I9edbfeafcf2b0450afc1845221944c0266db4597

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@ LOCAL_PATH := device/samsung/k3gxx
 PRODUCT_PACKAGES += \
 	fstab.universal5422 \
 	init.recovery.universal5422.rc \
+        init.baseband.rc \
 	init.samsung.rc \
 	init.universal5422.rc \
 	init.universal5422.usb.rc \
